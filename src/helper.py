13040b424860b3c8b35ac434fabf4b316df83ef5
alwaysonfire100-commits <alwaysonfire100@gmail.com>
2026-02-17 21:23:35 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,5 @@
 import fitz # PyMuPDF
-import os 
-from dotenv import load_dotenv
-from openai import OpenAI
+
 
 
 
@@ -28,33 +26,7 @@ def extract_text_from_pdf(uploaded_file):
 
 
 
-def (prompt, max_tokens=500):
-    """
-    Sends a prompt to the OpenAI API and returns the response.
-    
-    Args:
-        prompt (str): The prompt to send to the OpenAI API.
-        model (str): The model to use for the request.
-        temperature (float): The temperature for the response.
-        
-    Returns:
-        str: The response from the OpenAI API.
-    """
-    
 
-    response = client.chat.completions.create(
-        model= "gpt-4o",
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        temperature=0.5,
-        max_tokens=max_tokens
-    )
-
-    return response.choices[0].message.content
 
 
 
