c1da9c7157e4337a9244d5352e3f7529692fab83
alwaysonfire100-commits <alwaysonfire100@gmail.com>
2026-02-17 21:09:27 +0530

    Update job_api.py

--- src/job_api.py
+++ src/job_api.py
@@ -1,7 +1,6 @@
 from apify_client import ApifyClient
 import os 
-from dotenv import load_dotenv
-load_dotenv()
+
 
 apify_client = ApifyClient(os.getenv("APIFY_API_TOKEN"))
 
@@ -33,3 +32,4 @@ def fetch_naukri_jobs(search_query, location = "india", rows=60):
     run = apify_client.actor("alpcnRV9YI9lYVPWk").call(run_input=run_input)
     jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
     return jobs
+
